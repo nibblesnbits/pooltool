48cc83e9ca2c92fe3b1a823603034b21022cd405
Stephen Collins <sgcollins@outlook.com>
2023-06-02 23:43:27 -0400

    added title editing

--- App.tsx
+++ App.tsx
@@ -6,18 +6,36 @@ import {
   Text,
   TouchableOpacity,
   View,
+  TextInput,
 } from 'react-native';
 import Counter from './Counter';
 
 function App(): JSX.Element {
-  const [counters, setCounters] = useState([{ title: 'Counter', id: 1 }]);
+  const [counters, setCounters] = useState([
+    { title: 'Counter', id: 1, editing: false },
+  ]);
 
   const addCounter = () => {
-    setCounters((v) => [...v, { title: 'Counter', id: v.length + 1 }]);
+    setCounters((v) => [
+      ...v,
+      { title: 'Counter', id: v.length + 1, editing: false },
+    ]);
   };
 
   const removeCounter = (id: number) => () => {
     setCounters((v) => v.filter((c) => c.id !== id));
+  };
+
+  const toggleEdit = (id: number) => () => {
+    setCounters((v) =>
+      v.map((c) => (c.id === id ? { ...c, editing: true } : c))
+    );
+  };
+
+  const editTitle = (id: number, title: string) => {
+    setCounters((v) =>
+      v.map((c) => (c.id === id ? { ...c, editing: false, title } : c))
+    );
   };
 
   return (
@@ -30,7 +48,24 @@ function App(): JSX.Element {
         </View>
         {counters.map((c) => (
           <View key={c.id}>
-            <Text>{c.title}</Text>
+            {c.editing ? (
+              <TouchableOpacity onPress={toggleEdit(c.id)}>
+                <Text style={styles.counterTitle}>{c.title}</Text>
+              </TouchableOpacity>
+            ) : (
+              <TextInput
+                style={styles.counterTitle}
+                value={c.title}
+                onChangeText={(title) => {
+                  const last = title.slice(-1);
+                  if (last !== '\n') {
+                    return editTitle(c.id, title);
+                  }
+                  editTitle(c.id, title.slice(0, -1));
+                  toggleEdit(c.id)();
+                }}
+              />
+            )}
             <Counter deleteHandler={removeCounter(c.id)} />
           </View>
         ))}
@@ -53,7 +88,10 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     textAlign: 'center',
-    fontSize: 32,
+    fontSize: 24,
+  },
+  counterTitle: {
+    fontSize: 24,
   },
 });
 
